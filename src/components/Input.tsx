1ba71635dbfaa54e0d1bd0227734a604fa6b4e92
minjeong9919 <minjeong9919@naver.com>
2024-12-19 14:38:02 +0900

    feat: 로그인 프레임 구현 및 react-hook-form 연결

---
+++ src/components/Input.tsx
@@ -0,0 +1,26 @@
+import { InputHTMLAttributes, forwardRef } from "react";
+
+interface PropsType extends InputHTMLAttributes<HTMLInputElement> {
+  title?: string;
+  errorMessage?: string;
+}
+
+const Input = forwardRef<HTMLInputElement, PropsType>(
+  ({ title, errorMessage, ...rest }, ref) => {
+    return (
+      <div className='w-full flex flex-col gap-3 mb-5 font-bold'>
+        {title}
+        <input
+          className='border-2 border-primary rounded py-1 px-2'
+          ref={ref}
+          {...rest}
+        />
+        {errorMessage && (
+          <div className='text-xs text-red font-medium'>{errorMessage}</div>
+        )}
+      </div>
+    );
+  }
+);
+
+export default Input;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#216FE4",
+        gray: "#B2B2B2",
+        red: "#FF0000",
+      },
+    },
   },
   plugins: [],
 };
