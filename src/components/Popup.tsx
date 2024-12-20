d213c345317ed7c05b37416dc47abdf3b00613f7
minjeong9919 <minjeong9919@naver.com>
2024-12-20 16:51:02 +0900

    feat: 팝업 컴포넌트에 useOutSideClick 훅 적용

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from "react";
 import { DataType } from "../types/dataTypes";
 
 interface PropsType {
@@ -5,37 +6,41 @@ interface PropsType {
   data: DataType;
 }
 
-export const Popup = ({ onClose, data }: PropsType) => {
-  console.log(data);
-  const { apiNm, apiCd, callTime, mdulCustCd, mdulNm } = data;
-  return (
-    <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center'>
-      <div className='bg-white rounded p-10 flex flex-col justify-center items-center'>
-        <h2 className='text-4xl mb-4 font-bold'>{apiNm}</h2>
-        <ol className='flex flex-col gap-2 mb-5'>
-          <li>
-            <strong>호출 시간:</strong> {callTime?.toString()}
-          </li>
-          <li>
-            <strong>API 이름:</strong> {apiNm}
-          </li>
-          <li>
-            <strong>API 코드:</strong> {apiCd}
-          </li>
-          <li>
-            <strong>모듈 코드:</strong> {mdulCustCd}
-          </li>
-          <li>
-            <strong>모듈 이름:</strong> {mdulNm}
-          </li>
-        </ol>
-        <button
-          onClick={() => onClose(false)}
-          className='bg-primary text-white rounded w-full py-2 text-xl hover:bg-primary100'
+export const Popup = forwardRef<HTMLDivElement, PropsType>(
+  ({ onClose, data }, ref) => {
+    const { apiNm, apiCd, callTime, mdulCustCd, mdulNm } = data;
+    return (
+      <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center'>
+        <div
+          ref={ref}
+          className='bg-white rounded p-10 flex flex-col justify-center items-center '
         >
-          닫기
-        </button>
+          <h2 className='text-4xl mb-4 font-bold'>{apiNm}</h2>
+          <ol className='flex flex-col gap-2 mb-5'>
+            <li>
+              <strong>호출 시간:</strong> {callTime?.toString()}
+            </li>
+            <li>
+              <strong>API 이름:</strong> {apiNm}
+            </li>
+            <li>
+              <strong>API 코드:</strong> {apiCd}
+            </li>
+            <li>
+              <strong>모듈 코드:</strong> {mdulCustCd}
+            </li>
+            <li>
+              <strong>모듈 이름:</strong> {mdulNm}
+            </li>
+          </ol>
+          <button
+            onClick={() => onClose(false)}
+            className='bg-primary text-white rounded w-full py-2 text-xl hover:bg-primary100'
+          >
+            닫기
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
