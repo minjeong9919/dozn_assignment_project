13b5c2e1770b717ac0514f1cfc216ad62498d116
minjeong9919 <minjeong9919@naver.com>
2024-12-20 17:47:58 +0900

    refactor: 팝업차에 스크래핑 데이터 연결

--- src/types/dataTypes.ts
+++ src/types/dataTypes.ts
@@ -17,13 +17,50 @@ export interface DataType {
   callTime?: Date;
 }
 
-export interface DataListResponseType {
-  errYn: string;
+export interface ResponseType {
+  errYn: "Y" | "N";
   code: string;
   msg: string;
+}
+
+export interface ScrappingDataType {
+  byteLength: string;
+  code: string;
+  data: [];
+  msg: string;
+}
+
+export interface DataListResponseType extends ResponseType {
   data: {
     list: DataType[];
     totalCount: string;
     totalPage: string;
+  };
+}
+
+export interface ScrappingDataResonseType extends ResponseType {
+  data: {
+    out: {
+      code: string;
+      data: {
+        idLogin: {
+          code: string;
+          msg: string;
+        };
+        logout: {
+          code: string;
+          msg: string;
+        };
+      } & Record<
+        string,
+        {
+          byteLength: string;
+          code: string;
+          data: object;
+          msg: string;
+        }
+      >;
+    };
+    msg: string;
   };
 }
