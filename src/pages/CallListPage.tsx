b8e9d1828caee3b91b48128458091003b6e7c563
minjeong9919 <minjeong9919@naver.com>
2024-12-20 23:50:08 +0900

    feat: 호출 히스토리 카드 컴포넌트 구현 및. 데이터 적용

--- src/pages/CallListPage.tsx
+++ src/pages/CallListPage.tsx
@@ -1,5 +1,17 @@
+import { Card } from "../components/Card";
+import { DataType } from "../types/dataTypes";
+
 const CallListPage = () => {
-  return <div>CallListPage</div>;
+  const rawHistory = localStorage.getItem("history");
+  const data = rawHistory ? JSON.parse(rawHistory) : [];
+
+  return (
+    <div className='grid grid-cols-3 gap-10 px-8'>
+      {data.map((callData: DataType) => (
+        <Card data={callData} />
+      ))}
+    </div>
+  );
 };
 
 export default CallListPage;
